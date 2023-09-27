32f1eff89d22846a209775dbcb75c2c6e9c0abe9
jonkspanda <jionte@gmail.com>
2023-09-27 16:28:31 +0200

    added classes and created requirements-file

---
+++ bot.py
@@ -0,0 +1,13 @@
+from ics import Calendar, Event
+
+
+class bot:
+
+    def __init__(self, token) -> None:
+        self.token = token
+
+    def init_bot(self):
+        pass
+
+    def generate_ics(self):
+        pass
