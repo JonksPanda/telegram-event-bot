4af0ec99742db193840d141ddae10efdbd521e61
jonkspanda <jionte@gmail.com>
2023-09-29 15:36:27 +0200

    added functionality to send .ics-file and removed message.py

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 from ics import Calendar, Event
-import logging
 
 from telegram import Update
 from telegram.ext import ContextTypes, ApplicationBuilder, CommandHandler
@@ -10,8 +9,11 @@ class Bot:
     def __init__(self, token) -> None:
         self.token = token
         self.app = ApplicationBuilder().token(self.token).build()
-        start_handler = CommandHandler('test', self.com_test)
-        self.app.add_handler(start_handler)
+        test_handler = CommandHandler('test', self.com_test)
+        newevent_handler = CommandHandler('newevent', self.com_newevent)
+
+        self.app.add_handler(test_handler)
+        self.app.add_handler(newevent_handler)
 
     def run_bot(self):
         self.app.run_polling()
@@ -19,5 +21,32 @@ class Bot:
     async def com_test(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         await context.bot.send_message(chat_id=update.effective_chat.id, text="hello world!")
 
-    # def newEvent(self):
-    #     pass
+    # Handles logic behind newevent command
+    async def com_newevent(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        # placeholder for now
+        event_msg = """
+        my event
+
+        some description here bla bla bla
+
+        time: 10/10 12:00:00
+
+        place: stockholm
+        """
+        # responds to newevent with an .ics file
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=event_msg)
+        await context.bot.send_document(chat_id=update.effective_chat.id, document=open(self.generate_ics(), "rb"))
+
+    def generate_ics(self):
+        calendar = Calendar()
+        event = Event()
+        event.name = "test"
+        event.organizer = "test"
+        event.begin = "2023-10-10 12:00:00"
+        event.end = "2023-10-10 13:00:00"
+        calendar.events.add(event)
+        print(calendar.events)
+        filepath = f"data/events/{event.name}.ics"
+        with open(filepath, "w") as f:
+            f.writelines(calendar.serialize_iter())
+        return filepath
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from bot import Bot
-import message
-from ics import Calendar, Event
+
+import logging
 
 
 # const of project directory
@@ -9,16 +9,9 @@ DIR = None
 
 def main():
 
-    # Temporary calendar-file creation for testing purposes
-    # calendar = Calendar()
-    # event = Event()
-    # event.name = "test"
-    # event.begin = "2023-10-10 12:00:00"
-    # event.end = "2023-10-10 13:00:00"
-    # calendar.events.add(event)
-    # print(calendar.events)
-    # with open(f"data/events/{event.name}.ics", "w") as f:
-    #     f.writelines(calendar.serialize_iter())
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
     with open("config/token.txt") as f:
         token = f.read()
     print(token)
