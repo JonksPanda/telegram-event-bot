e923b2385f253c20a7947d7c7b4005e0882bbb7e
jonkspanda <jionte@gmail.com>
2023-09-29 11:28:58 +0200

    testing telegram bot

--- bot.py
+++ bot.py
@@ -1,14 +1,23 @@
 from ics import Calendar, Event
-import telegram
+import logging
+
+from telegram import Update
+from telegram.ext import ContextTypes, ApplicationBuilder, CommandHandler
 
 
-class bot:
+class Bot:
 
     def __init__(self, token) -> None:
         self.token = token
+        self.app = ApplicationBuilder().token(self.token).build()
+        start_handler = CommandHandler('test', self.com_test)
+        self.app.add_handler(start_handler)
 
-    def init_bot(self):
-        pass
+    def run_bot(self):
+        self.app.run_polling()
 
-    def generate_ics(self):
-        pass
+    async def com_test(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="hello world!")
+
+    # def newEvent(self):
+    #     pass
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import bot
+from bot import Bot
 import message
 from ics import Calendar, Event
 
@@ -10,15 +10,20 @@ DIR = None
 def main():
 
     # Temporary calendar-file creation for testing purposes
-    calendar = Calendar()
-    event = Event()
-    event.name = "test"
-    event.begin = "2023-10-10 12:00:00"
-    event.end = "2023-10-10 13:00:00"
-    calendar.events.add(event)
-    print(calendar.events)
-    with open(f"data/events/{event.name}.ics", "w") as f:
-        f.writelines(calendar.serialize_iter())
+    # calendar = Calendar()
+    # event = Event()
+    # event.name = "test"
+    # event.begin = "2023-10-10 12:00:00"
+    # event.end = "2023-10-10 13:00:00"
+    # calendar.events.add(event)
+    # print(calendar.events)
+    # with open(f"data/events/{event.name}.ics", "w") as f:
+    #     f.writelines(calendar.serialize_iter())
+    with open("config/token.txt") as f:
+        token = f.read()
+    print(token)
+    chat_bot = Bot(token)
+    chat_bot.run_bot()
 
 
 if __name__ == "__main__":
