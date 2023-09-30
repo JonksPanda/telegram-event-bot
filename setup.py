1d8d392d21c90e99e48814effe7bf0272ce869e8
jonkspanda <jionte@gmail.com>
2023-09-30 18:27:25 +0200

    added config file creation

--- setup.py
+++ setup.py
@@ -6,17 +6,21 @@ import os.path
 
 class Config:
     def __init__(self, token=None) -> None:
-        self.YAML_PATH = "/config/conf.yaml"
+        self.YAML_PATH = "./config/conf.yaml"
         self.yaml_exists = os.path.exists(self.YAML_PATH)
         self.token = token
 
         if self.yaml_exists:
+            print("reading settings from config file..")
             self.read_yaml()
         else:
+            print("no config file found! Generating new file on /config/conf.yaml")
             self.create_yaml()
 
     def read_yaml(self):
         pass
 
     def create_yaml(self):
-        pass
+        yaml_input = [{"token": self.token}]
+        with open(self.YAML_PATH, "w") as file:
+            yaml.dump(yaml_input, file)
