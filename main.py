e741eff110b5f98db8cb61e5f5be27716abc4281
jonkspanda <jionte@gmail.com>
2023-09-30 17:58:41 +0200

    added log file

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ DIR = None
 def main():
 
     logging.basicConfig(
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO, encoding="utf-8", filename=f"data/logs/event_log.log")
 
     with open("config/token.txt") as f:
         token = f.read()
