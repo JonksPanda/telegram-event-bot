3e3b15d93d6d827994b2793f846c121aa28d19bb
jonkspanda <jionte@gmail.com>
2023-10-02 16:53:34 +0200

    added function that reads config file and puts settings in dictionaries

--- main.py
+++ main.py
@@ -10,15 +10,12 @@ DIR = None
 
 def main():
 
+    # Add ability to put token as argument
+
     logging.basicConfig(
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO, encoding="utf-8", filename=f"./data/logs/event_log.log")
     conf = Config()
-    print(conf.yaml_exists)
-
-    with open("./config/token.txt") as f:
-        token = f.read()
-    print(token)
-    chat_bot = Bot(token)
+    chat_bot = Bot(conf.credentials['token'])
     chat_bot.run_bot()
 
 
--- setup.py
+++ setup.py
@@ -9,20 +9,27 @@ class Config:
         self.YAML_PATH = "./config/conf.yaml"
         self.yaml_exists = os.path.exists(self.YAML_PATH)
         self.token = token
+        self.credentials = None
+        self.paths = None
 
         if self.yaml_exists:
-            self.read_yaml()
+            self.credentials, self.paths = self.read_yaml()
         else:
             self.create_yaml()
+            self.credentials, self.paths = self.read_yaml()
+        self.credentials = self.credentials["credentials"]
+        self.paths = self.paths["paths"]
 
     def read_yaml(self):
         print("reading settings from config file..")
         with open(self.YAML_PATH, "r") as f:
-            print(yaml.load(f, Loader=yaml.FullLoader))
+            yaml_out = yaml.load(f, Loader=yaml.FullLoader)
+            print(yaml_out)
+            return yaml_out
 
     def create_yaml(self):
         print("no config file found! Generating new file on ./config/conf.yaml")
         yaml_input = [{"credentials": {"token": self.token}}]
         with open(self.YAML_PATH, "w") as f:
             yaml.dump(yaml_input, f)
-        self.read_yaml()
+        self.yaml_exists = os.path.exists(self.YAML_PATH)
